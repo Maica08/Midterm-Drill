88d5b177aadc97bb18375252be8af2c2e18c0823
PSU-Maica <202280193@psu.palawan.edu.ph>
2023-11-16 10:35:20 +0800

    Revision 1

--- midtermlab.py
+++ midtermlab.py
@@ -8,8 +8,7 @@ class Account:
 
     @property
     def accountNumber(self) -> int:
-        account_number = self._accountNumber
-        return account_number
+        return self._accountNumber
 
     @accountNumber.setter
     def accountNumber(self, new_accNumber:int) -> None:
@@ -17,8 +16,7 @@ class Account:
 
     @property
     def balance(self) -> float:
-        balance = self._balance
-        return balance
+        return self._balance
 
     @balance.setter
     def balance(self, set_balance:float) -> None:
@@ -26,13 +24,11 @@ class Account:
 
     @property
     def dateofOpening(self) -> str:
-        date = self._dateofOpening
-        return date
+        return self._dateofOpening
 
     @dateofOpening.setter
     def dateofOpening(self, set_date:str) -> None:
         self._dateofOpening = set_date
-
 
     def debit_amount(self, withdraw_amount:float) -> None:
         if withdraw_amount > self.balance:
@@ -56,8 +52,7 @@ class Customer:
     
     @property
     def customerId(self) -> str:
-        customerId = self._customerId
-        return customerId
+        return self._customerId
 
     @customerId.setter
     def customerId(self, set_customerId:str) -> None:
@@ -65,8 +60,7 @@ class Customer:
 
     @property
     def name(self) -> str:
-        name = self._name
-        return name
+        return self._name
 
     @name.setter
     def name(self, set_name:str) -> None:
@@ -74,8 +68,7 @@ class Customer:
 
     @property
     def address(self) -> str:
-        address = self._address
-        return address
+        return self._address
 
     @address.setter
     def address(self, set_address:str) -> None:
@@ -83,8 +76,7 @@ class Customer:
 
     @property
     def phone(self) -> str:
-        phone = self._phone
-        return phone
+        return self._phone
 
     @address.setter
     def phone(self, set_phone:str) -> None:
@@ -105,28 +97,30 @@ class SavingsAccount(Account):
     def __init__(self, accountNumber:int, balance:float, dateofOpening:str, minBalance:float):
         super().__init__(accountNumber, balance, dateofOpening)
         self._minBalance = minBalance
+        self.customer = None
+
+        SavingsAccount.all_savings_accounts.append(self)
 
     @property
     def minBalance(self) -> float:
-        minBalance = self._minBalance
-        return minBalance
+        return self._minBalance
 
     @minBalance.setter
     def minBalance(self, set_minBalance:float) -> None:
         self._minBalance = set_minBalance
 
     def add_customer(self, customer:Customer):
-        for account in self.all_savings_accounts:
-            if account["customer"] == customer:
-                print("Cannot add account; account already exists.")
-            
-        self.customer = customer
-        account = {"customer": self.customer, "account number": self.accountNumber, "balance": self.balance, "date of opening": self.dateofOpening, "minimum balance": self.minBalance}
-        self.all_savings_accounts.append(account)
+        if self.customer == None:
+            self.customer = customer
+        else:
+            print("Customer already assigned.")
 
     def remove_customer(self) -> None:
-        self.customer = None
-
+        if self.customer != None:
+            self.customer = None
+        else:
+            print("Nothing to remove.")
+        
     
 class CurrentAccount(Account):
     all_current_accounts = []
@@ -134,6 +128,9 @@ class CurrentAccount(Account):
     def __init__(self, accountNumber: int, balance: float, dateofOpening: str, interestRate:float):
         super().__init__(accountNumber, balance, dateofOpening)
         self._interestRate = interestRate
+        self.customer = None
+
+        CurrentAccount.all_current_accounts.append(self)
 
     @property
     def interestRate(self) -> float:
@@ -144,67 +141,51 @@ class CurrentAccount(Account):
     def interestRate(self, set_interestRate:float) -> None:
         self._interestRate = set_interestRate
 
-    def add_customer(self, customer):
-        # if len(self.all_current_accounts) != 0:
-        for account in self.all_current_accounts:
-            if account["customer"] == customer:
-                print("Cannot add account; account already exists.")
-                return
-            
-        self.customer = customer
-        account = {"customer": self.customer, "account number": self.accountNumber, "balance": self.balance, "date of opening": self.dateofOpening, "interest rate": self.interestRate}
-        self.all_current_accounts.append(account)
-
+    def add_customer(self, customer:Customer) -> None:
+        if self.customer == None:
+            self.customer = customer
+        else:
+            print("Customer already assigned.")
 
     def remove_customer(self) -> None:
-        self.customer = None
+        if self.customer != None:
+            self.customer = None
+        else:
+            print("Nothing to remove.")
 
 
 def main():
     customer1 = Customer(customerId=100001, name="George Cruz", address="Palawan", phone="09123456789")
     customer2 = Customer(customerId=100002, name="Maya Magno", address="Puerto Princesa", phone="09345678912")
 
+    savings1 = SavingsAccount(accountNumber = 125456, balance = 4500, dateofOpening = "Jan 3, 2015", minBalance = 100)
+    savings2 = SavingsAccount(accountNumber = 125457, balance = 5500, dateofOpening = "Aug 3, 2015", minBalance = 100)
+    savings1.add_customer(customer1)
+    savings2.add_customer(customer2)
 
-    savings = SavingsAccount(accountNumber = 125456, balance = 4500, dateofOpening = "Jan 3, 2015", minBalance = 100)
-    savings.add_customer(customer1)
+    current1 = CurrentAccount(accountNumber=125456, balance=4500, dateofOpening="Jan 3, 2015", interestRate=0.02)
+    current2 = CurrentAccount(accountNumber=125457, balance=5500, dateofOpening="Sep 15, 2015", interestRate=0.05)
+    current1.add_customer(customer1)
+    current2.add_customer(customer2)
 
-    savings.remove_customer()
-    savings.accountNumber = 125457
-    savings.balance = 5500
-    savings.dateofOpening = "Aug 3, 2015"
-    savings.minBalance = 100
-    savings.add_customer(customer2)
+    print("----------Customer Info----------")
+    for account in SavingsAccount.all_savings_accounts:
+        account.customer.display_info()
+        print("Account Type: Savings")
+        print(f"Account Number: {account.accountNumber}")
+        print(f"Balance: {account.balance}")
+        print(f"Date of Opening: {account.dateofOpening}")
+        print(f"Minimum Balance: {account.minBalance}")
+        print()
 
-
-    # savings2 = SavingsAccount(accountNumber="125457", balance="5500", dateofOpening="Aug 3, 2015", minBalance=100)
-    # savings2.add_customer(customer2)
-
-    current = CurrentAccount(accountNumber=125456, balance="4500", dateofOpening="Jan 3, 2015", interestRate=0.02)
-    current.add_customer(customer1)
-
-    current.remove_customer()
-    current.accountNumber = 125457 
-    current.balance = 5500 
-    current.dateofOpening = "Jan 3, 2015"
-    current.interestRate = 0.03
-    current.add_customer(customer2)
-
-
-    customer1.display_info()
-    print()
-    customer2.display_info()
-    print()
-
-    # print(savings.all_savings_accounts)
-
-    for account in savings.all_savings_accounts:
-        print(account)
-    print()
-
-    for account in current.all_current_accounts:
-        print(account)
-
-    # print(savings.customer.display_info())
+    for account in CurrentAccount.all_current_accounts:
+        account.customer.display_info()
+        print("Account Type: Current")
+        print(f"Account Number: {account.accountNumber}")
+        print(f"Balance: {account.balance}")
+        print(f"Date of Opening: {account.dateofOpening}")
+        print(f"Interest Rate: {account.interestRate}")
+        print()
 
 
 if __name__ == "__main__":
